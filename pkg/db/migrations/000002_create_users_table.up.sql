dee055edd243cc3c1a8cf2c3761ccb237a08e8c2
Taras Kushnir <tk.dev@mailbox.org>
2024-01-11 09:30:40 +0200

    Use random uuid instead of next

--- pkg/db/migrations/000002_create_users_table.up.sql
+++ pkg/db/migrations/000002_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
-    external_id UUID DEFAULT uuid_generate_v4(),
+    external_id UUID DEFAULT gen_random_uuid(),
     user_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT current_timestamp,
     updated_at TIMESTAMPTZ DEFAULT current_timestamp
--- pkg/db/migrations/000003_create_properties_table.up.sql
+++ pkg/db/migrations/000003_create_properties_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS properties(
     id SERIAL PRIMARY KEY,
-    external_id UUID DEFAULT uuid_generate_v4(),
+    external_id UUID DEFAULT gen_random_uuid(),
     user_id INTEGER REFERENCES users(id),
     created_at TIMESTAMPTZ DEFAULT current_timestamp,
     updated_at TIMESTAMPTZ DEFAULT current_timestamp
