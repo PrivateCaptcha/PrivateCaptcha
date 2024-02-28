350e3e4dd27f7a372483d97db2d3e19434103ea7
Taras Kushnir <tk.dev@mailbox.org>
2024-02-28 17:22:04 +0200

    Return only non-expired items from cache

--- pkg/db/queries/postgres/cache.sql
+++ pkg/db/queries/postgres/cache.sql
@@ -1,5 +1,5 @@
 -- name: GetCachedByKey :one
-SELECT value FROM cache WHERE key = $1;
+SELECT value FROM cache WHERE key = $1 AND expires_at >= NOW();
 
 -- name: CreateCache :exec
 INSERT INTO cache (key, value, expires_at) VALUES ($1, $2, NOW() + $3::INTERVAL);
