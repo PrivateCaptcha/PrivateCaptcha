5e771d7e25354e47670ca3435c9ea0ebef7bd5eb
Taras Kushnir <tk.dev@mailbox.org>
2024-03-05 18:07:36 +0200

    Fix clickhouse image to 23.8.9

--- docker/run-clickhouse.sh
+++ docker/run-clickhouse.sh
@@ -9,8 +9,8 @@ docker run -d --rm \
     -p 9000:9000 \
     --ulimit nofile=262144:262144 \
     -e CLICKHOUSE_DB=privatecaptcha \
-    -v ./docker/clickhouse-config.xml:/etc/clickhouse-server/config.d/myconfig.xml \
-    clickhouse/clickhouse-server:23.8-alpine
+    -v $(pwd)/docker/clickhouse-config.xml:/etc/clickhouse-server/config.d/myconfig.xml \
+    clickhouse/clickhouse-server:23.8.9-alpine
 
 echo "Waiting for clickhouse healthcheck..."
 
