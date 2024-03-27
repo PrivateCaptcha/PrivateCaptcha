87d620f9c123a74a428c87c2ce3f7133b5157e5b
Taras Kushnir <tk.dev@mailbox.org>
2024-03-27 08:40:30 +0200

    Persist sessions in cache

--- pkg/db/queries/postgres/cache.sql
+++ pkg/db/queries/postgres/cache.sql
@@ -2,10 +2,15 @@
 SELECT value FROM cache WHERE key = $1 AND expires_at >= NOW();
 
 -- name: CreateCache :exec
-INSERT INTO cache (key, value, expires_at) VALUES ($1, $2, NOW() + $3::INTERVAL);
+INSERT INTO cache (key, value, expires_at) VALUES ($1, $2, NOW() + $3::INTERVAL)
+ON CONFLICT (key) DO UPDATE 
+SET value = EXCLUDED.value, expires_at = EXCLUDED.expires_at;
 
 -- name: UpdateCacheExpiration :exec
 UPDATE cache SET expires_at = NOW() + $2::INTERVAL WHERE key = $1;
+
+-- name: DeleteCachedByKey :exec
+DELETE FROM cache WHERE key = $1;
 
 -- name: DeleteExpiredCache :exec
 DELETE FROM cache WHERE expires_at < NOW();
--- pkg/session/common.go
+++ pkg/session/common.go
@@ -1,26 +1,9 @@
 package session
 
-import "time"
-
-type Key int
-
 const (
 	KeyLoginStep = iota
 	KeyUserEmail
 	KeyTwoFactorCode
 	KeyUserName
+	KeyPersistent
 )
-
-type Session interface {
-	Set(key Key, value interface{}) error
-	Get(key Key) interface{}
-	Delete(key Key) error
-	SessionID() string
-}
-
-type Provider interface {
-	SessionInit(sid string) (Session, error)
-	SessionRead(sid string) (Session, error)
-	SessionDestroy(sid string) error
-	SessionGC(d time.Duration)
-}
