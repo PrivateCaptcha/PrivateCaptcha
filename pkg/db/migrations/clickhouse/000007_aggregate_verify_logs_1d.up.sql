663e59ac14baefbb9588752a2d46ae2c7c38f216
Taras Kushnir <tk.dev@mailbox.org>
2024-04-24 21:24:43 +0300

    Save verify requests to DB

---
+++ pkg/db/migrations/clickhouse/000007_aggregate_verify_logs_1d.up.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS privatecaptcha.verify_logs_1d
+(
+    user_id UInt32,
+    org_id UInt32,
+    property_id UInt32,
+    timestamp DateTime,
+    count UInt64
+)
+ENGINE = SummingMergeTree
+ORDER BY (user_id, org_id, property_id, timestamp)
+TTL timestamp + INTERVAL 1 MONTH;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS privatecaptcha.verify_logs_1d_mv TO privatecaptcha.verify_logs_1d AS
+SELECT
+    user_id,
+    org_id,
+    property_id,
+    toStartOfDay(timestamp) AS timestamp,
+    sum(count) AS count
+FROM privatecaptcha.verify_logs_1h
+GROUP BY user_id, org_id, property_id, timestamp;
