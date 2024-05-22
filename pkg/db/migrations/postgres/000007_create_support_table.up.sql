ca85eb295b0c669dfd7d9484f11693249a9bcbd4
Taras Kushnir <tk.dev@mailbox.org>
2024-05-22 15:43:26 +0300

    Add resolution field to support table

--- pkg/db/migrations/postgres/000007_create_support_table.up.sql
+++ pkg/db/migrations/postgres/000007_create_support_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS support(
     id SERIAL PRIMARY KEY,
     category support_category NOT NULL,
     message TEXT DEFAULT '',
+    resolution TEXT DEFAULT '',
     user_id INTEGER REFERENCES users(id),
     created_at TIMESTAMPTZ DEFAULT current_timestamp
 );
