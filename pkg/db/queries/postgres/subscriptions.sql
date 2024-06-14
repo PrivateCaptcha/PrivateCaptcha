566359e4f30c26f6825370c412a3a0e67a91e263
Taras Kushnir <tk.dev@mailbox.org>
2024-06-14 16:27:08 +0300

    Record cancelled from date

--- pkg/db/queries/postgres/subscriptions.sql
+++ pkg/db/queries/postgres/subscriptions.sql
@@ -5,4 +5,4 @@ SELECT * FROM subscriptions WHERE id = $1;
 INSERT INTO subscriptions (paddle_product_id, paddle_price_id, paddle_subscription_id, paddle_customer_id, status, trial_ends_at, next_billed_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;
 
 -- name: UpdateSubscription :one
-UPDATE subscriptions SET paddle_product_id = $2, status = $3, next_billed_at = $4, updated_at = NOW() WHERE paddle_subscription_id = $1 RETURNING *;
+UPDATE subscriptions SET paddle_product_id = $2, status = $3, next_billed_at = $4, cancel_from = $5, updated_at = NOW() WHERE paddle_subscription_id = $1 RETURNING *;
--- pkg/db/queries/postgres/users.sql
+++ pkg/db/queries/postgres/users.sql
@@ -7,7 +7,7 @@ INSERT INTO users (name, email, subscription_id) VALUES ($1, $2, $3) RETURNING *
 -- name: UpdateUserData :one
 UPDATE users SET name = $2, email = $3, updated_at = NOW() WHERE id = $1 RETURNING *;
 
--- name: UpdateUserSubscription :exec
+-- name: UpdateUserSubscription :one
 UPDATE users SET subscription_id = $2, updated_at = NOW() WHERE id = $1 RETURNING *;
 
 -- name: SoftDeleteUser :exec
