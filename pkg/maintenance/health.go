fb5f45b2ca24349c16b88c18725c0e889186c9c2
Taras Kushnir <tk.dev@mailbox.org>
2024-11-06 11:11:01 +0200

    Process signals explicitly and change health status immediately on exit

--- pkg/maintenance/health.go
+++ pkg/maintenance/health.go
@@ -15,18 +15,21 @@ import (
 )
 
 type HealthCheckJob struct {
-	BusinessDB     *db.BusinessStore
-	TimeSeriesDB   *db.TimeSeriesStore
-	Router         *http.ServeMux
-	postgresFlag   atomic.Int32
-	clickhouseFlag atomic.Int32
-	CheckInterval  time.Duration
-	WithSystemd    bool
+	BusinessDB       *db.BusinessStore
+	TimeSeriesDB     *db.TimeSeriesStore
+	Router           *http.ServeMux
+	postgresFlag     atomic.Int32
+	clickhouseFlag   atomic.Int32
+	shuttingDownFlag atomic.Int32
+	CheckInterval    time.Duration
+	WithSystemd      bool
 }
 
 const (
 	greenPage = `<!DOCTYPE html><html><body style="background-color: green;"></body></html>`
 	redPage   = `<!DOCTYPE html><html><body style="background-color: red;"></body></html>`
+	flagTrue  = 1
+	flagFalse = 0
 )
 
 var _ common.PeriodicJob = (*HealthCheckJob)(nil)
@@ -52,7 +55,7 @@ func (hc *HealthCheckJob) RunOnce(ctx context.Context) error {
 	hc.clickhouseFlag.Store(hc.checkClickHouse(ctx))
 
 	if hc.WithSystemd {
-		if result := hc.checkHTTP(ctx); result == 1 {
+		if result := hc.checkHTTP(ctx); result == flagTrue {
 			_, _ = daemon.SdNotify(false, daemon.SdNotifyWatchdog)
 		}
 	}
@@ -61,9 +64,9 @@ func (hc *HealthCheckJob) RunOnce(ctx context.Context) error {
 }
 
 func (hc *HealthCheckJob) checkClickHouse(ctx context.Context) int32 {
-	result := int32(0)
+	result := int32(flagFalse)
 	if err := hc.TimeSeriesDB.Ping(ctx); err == nil {
-		result = 1
+		result = flagTrue
 	} else {
 		slog.ErrorContext(ctx, "Failed to ping ClickHouse", common.ErrAttr(err))
 	}
@@ -71,9 +74,9 @@ func (hc *HealthCheckJob) checkClickHouse(ctx context.Context) int32 {
 }
 
 func (hc *HealthCheckJob) checkPostgres(ctx context.Context) int32 {
-	result := int32(0)
+	result := int32(flagFalse)
 	if err := hc.BusinessDB.Ping(ctx); err == nil {
-		result = 1
+		result = flagTrue
 	} else {
 		slog.ErrorContext(ctx, "Failed to ping Postgres", common.ErrAttr(err))
 	}
@@ -81,15 +84,19 @@ func (hc *HealthCheckJob) checkPostgres(ctx context.Context) int32 {
 }
 
 func (hc *HealthCheckJob) isPostgresHealthy() bool {
-	return hc.postgresFlag.Load() == 1
+	return hc.postgresFlag.Load() == flagTrue
 }
 
 func (hc *HealthCheckJob) isClickHouseHealthy() bool {
-	return hc.clickhouseFlag.Load() == 1
+	return hc.clickhouseFlag.Load() == flagTrue
+}
+
+func (hc *HealthCheckJob) isShuttingDown() bool {
+	return hc.shuttingDownFlag.Load() == flagTrue
 }
 
 func (hc *HealthCheckJob) checkHTTP(ctx context.Context) int32 {
-	result := int32(0)
+	result := int32(flagFalse)
 	req, err := http.NewRequest(http.MethodGet, "/"+common.HealthEndpoint, nil)
 	if err != nil {
 		slog.ErrorContext(ctx, "Failed to ping own health endpoint", common.ErrAttr(err))
@@ -99,15 +106,21 @@ func (hc *HealthCheckJob) checkHTTP(ctx context.Context) int32 {
 	hc.Router.ServeHTTP(w, req)
 	resp := w.Result()
 	if resp.StatusCode == http.StatusOK {
-		result = 1
+		result = flagTrue
 	}
 	return result
+}
+
+func (hc *HealthCheckJob) Shutdown(ctx context.Context) {
+	slog.DebugContext(ctx, "Shutting down health check job")
+	hc.shuttingDownFlag.Store(flagTrue)
 }
 
 func (hc *HealthCheckJob) HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(common.HeaderContentType, common.ContentTypeHTML)
 	healthy := hc.isPostgresHealthy() && hc.isClickHouseHealthy()
-	if healthy {
+	shuttingDown := hc.isShuttingDown()
+	if healthy && !shuttingDown {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, greenPage)
 	} else {
