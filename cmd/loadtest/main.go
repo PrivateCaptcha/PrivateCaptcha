80a2ed4df221a1df586f448de560398d76edffce
Taras Kushnir <tk.dev@mailbox.org>
2024-11-21 18:48:41 +0200

    Add code to run a loadtest. related #98

---
+++ cmd/loadtest/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	modeSeed = "seed"
+	modeTest = "test"
+)
+
+var (
+	envFileFlag         = flag.String("env", "", "Path to .env file")
+	flagMode            = flag.String("mode", "", strings.Join([]string{modeSeed, modeTest}, " | "))
+	flagUsersCount      = flag.Int("user-count", 100, "number of users to seed")
+	flagOrgsCount       = flag.Int("org-count", 10, "number of orgs to seed")
+	flagPropertiesCount = flag.Int("property-count", 100, "number of properties to seed")
+	flagRatePerSecond   = flag.Int("rps", 100, "Requests per second")
+	flagDuration        = flag.Int("duration", 10, "Duration of the load test (seconds)")
+)
+
+func main() {
+	flag.Parse()
+
+	var err error
+
+	if len(*envFileFlag) > 0 {
+		err = godotenv.Load(*envFileFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+	}
+
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
+	slog.SetDefault(logger)
+
+	switch *flagMode {
+	case modeSeed:
+		err = seed(*flagUsersCount, *flagOrgsCount, *flagPropertiesCount, os.Getenv)
+	case modeTest:
+		err = load((*flagUsersCount)*(*flagOrgsCount)*(*flagPropertiesCount), os.Getenv, *flagRatePerSecond, *flagDuration)
+	default:
+		err = fmt.Errorf("unknown mode: '%s'", *flagMode)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
--- pkg/leakybucket/manager_test.go
+++ pkg/leakybucket/manager_test.go
@@ -2,7 +2,9 @@ package leakybucket
 
 import (
 	"context"
+	"net/netip"
 	"slices"
+	"sync"
 	"testing"
 	"time"
 )
@@ -10,7 +12,7 @@ import (
 func TestManagerCleanup(t *testing.T) {
 	const maxBuckets = 8
 	const cap = 5
-	manager := NewManager[int32, ConstLeakyBucket[int32], *ConstLeakyBucket[int32]](maxBuckets, cap, 1*time.Second)
+	manager := NewManager[int32, ConstLeakyBucket[int32]](maxBuckets, cap, 1*time.Second)
 	tnow := time.Now().Truncate(1 * time.Second)
 	// we add in reverse to check that we cleanup in correct order
 	for i := maxBuckets - 1; i >= 0; i-- {
@@ -39,5 +41,115 @@ func TestManagerCleanup(t *testing.T) {
 	// oldest items should be the last in the list based on BucketsHeap
 	if !slices.Equal(deletedKeys, []int32{7, 6, 5, 4}) {
 		t.Errorf("Unexpected keys were deleted: %v", deletedKeys)
+	}
+}
+
+func TestManagerAdd(t *testing.T) {
+	const maxBuckets = 8
+	const cap = 5
+	const key = 123
+
+	manager := NewManager[int32, ConstLeakyBucket[int32]](maxBuckets, cap, 1*time.Second)
+	tnow := time.Now().Truncate(1 * time.Second)
+
+	for i := 0; i < cap; i++ {
+		result := manager.Add(key, 1, tnow)
+		if result.CurrLevel != uint32(i+1) {
+			t.Errorf("Unexpected level: %v", result.CurrLevel)
+		}
+		if result.Added != 1 {
+			t.Errorf("Failed to add to bucket")
+		}
+	}
+}
+
+func TestManagerAddParallel(t *testing.T) {
+	const maxBuckets = 8
+	const cap = 5
+	const key = 123
+
+	manager := NewManager[int32, ConstLeakyBucket[int32]](maxBuckets, cap, 1*time.Second)
+	tnow := time.Now().Truncate(1 * time.Second)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < cap; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			result := manager.Add(key, 1, tnow)
+			if result.Added != 1 {
+				t.Errorf("Failed to add to bucket")
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	result := manager.Add(key, 1, tnow)
+	if result.CurrLevel != cap {
+		t.Errorf("Unexpected level after full: %v", result.CurrLevel)
+	}
+	if result.Added != 0 {
+		t.Errorf("Was able to add to the bucket after")
+	}
+}
+
+func TestManagerAddDefault(t *testing.T) {
+	const maxBuckets = 8
+	const cap = 5
+	const key = 123
+
+	manager := NewManager[int32, ConstLeakyBucket[int32]](maxBuckets, cap, 1*time.Second)
+	tnow := time.Now().Truncate(1 * time.Second)
+
+	manager.SetDefaultBucket(NewConstBucket[int32](key, cap, 1*time.Second, tnow.Add(-1*time.Minute)))
+
+	for i := 0; i < cap; i++ {
+		result := manager.Add(key, 1, tnow)
+		if result.CurrLevel != uint32(i+1) {
+			t.Errorf("Unexpected level: %v", result.CurrLevel)
+		}
+		if result.Added != 1 {
+			t.Errorf("Failed to add to bucket")
+		}
+	}
+
+	result := manager.Add(key, 1, tnow)
+	if result.CurrLevel != cap {
+		t.Errorf("Unexpected level after full: %v", result.CurrLevel)
+	}
+	if result.Added != 0 {
+		t.Errorf("Managed to add to full bucket")
+	}
+}
+
+func TestManagerIPAddrAddDefault(t *testing.T) {
+	const maxBuckets = 8
+	const cap = 5
+	manager := NewManager[netip.Addr, ConstLeakyBucket[netip.Addr]](maxBuckets, cap, 1*time.Second)
+
+	tnow := time.Now().Truncate(1 * time.Second)
+	manager.SetDefaultBucket(NewConstBucket(netip.Addr{}, cap, 1.0 /*leakRatePerSecond*/, tnow))
+
+	key := netip.Addr{}
+	for i := 0; i < cap; i++ {
+		result := manager.Add(key, 1, tnow)
+		if result.CurrLevel != uint32(i+1) {
+			t.Errorf("Unexpected level: %v", result.CurrLevel)
+		}
+		if result.Added != 1 {
+			t.Errorf("Failed to add to bucket")
+		}
+	}
+
+	result := manager.Add(key, 1, tnow)
+	if result.CurrLevel != cap {
+		t.Errorf("Unexpected level after full: %v", result.CurrLevel)
+	}
+	if result.Added != 0 {
+		t.Errorf("Managed to add to full bucket")
 	}
 }
