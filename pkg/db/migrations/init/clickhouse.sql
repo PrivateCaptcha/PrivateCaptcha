66d0163e25e02ceee86f4476bd57c6f4705a3758
Taras Kushnir <tk.dev@mailbox.org>
2025-02-07 21:18:21 +0200

    Cleanup clickhouse init script [ci skip]

--- pkg/db/migrations/init/clickhouse.sql
+++ pkg/db/migrations/init/clickhouse.sql
@@ -1,6 +1,3 @@
-CREATE USER captcha_admin IDENTIFIED BY 'foOn4DMt19zwvSoL';
-GRANT ALL ON *.* TO captcha_admin WITH GRANT OPTION;
-
 CREATE ROLE pc_backend_role;
 GRANT SELECT, INSERT, DELETE ON privatecaptcha.* TO pc_backend_role;
 GRANT ALTER DELETE ON privatecaptcha.* to pc_backend_role;
