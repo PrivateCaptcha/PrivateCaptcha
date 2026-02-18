81cbd6211085d3aa7a3db544f32f233d4f4f385b
Taras Kushnir <tk.dev@mailbox.org>
2026-02-18 11:44:04 +0200

    Cosmetic improvement

--- pkg/portal/property_enterprise.go
+++ pkg/portal/property_enterprise.go
@@ -170,9 +170,9 @@ func (s *Server) getPropertyRules(w http.ResponseWriter, r *http.Request) (*prop
 	// Check for cached org rules and show info message if they exist
 	if orgRules, err := s.Store.Impl().GetCachedOrgRules(ctx, org.ID); err == nil && len(orgRules) > 0 {
 		if len(orgRules) == 1 {
-			renderCtx.InfoMessage = "1 org rule is also applied to this property."
+			renderCtx.InfoMessage = "1 organization rule is also applied to this property."
 		} else {
-			renderCtx.InfoMessage = fmt.Sprintf("%d org rules are also applied to this property.", len(orgRules))
+			renderCtx.InfoMessage = fmt.Sprintf("%d organization rules are also applied to this property.", len(orgRules))
 		}
 	}
 
