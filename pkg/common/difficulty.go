c02505cea3fb753d8dd271f43a4d84be7981fe9a
Taras Kushnir <tk.dev@mailbox.org>
2026-03-01 16:09:06 +0100

    Fix difficulty threshold computations on the client

--- pkg/common/difficulty.go
+++ pkg/common/difficulty.go
@@ -3,8 +3,10 @@ package common
 type DifficultyLevel uint8
 
 const (
-	// NOTE: We want them equally spaced
-	DifficultyDelta                       = 15
+	// difficulty scales on an exponential curve defined by 2^(d/8) so 1 step of
+	// difficulty change incurrs 2^(1/8) = 1.0905077 jump in computations (9.05%)
+	// so to get +100% computations we use 8 difficulty steps (so +16 steps is 200%)
+	DifficultyDelta                       = 16
 	DifficultyLevelSmall  DifficultyLevel = 80
 	DifficultyLevelMedium DifficultyLevel = DifficultyLevelSmall + DifficultyDelta
 	DifficultyLevelHigh   DifficultyLevel = DifficultyLevelMedium + DifficultyDelta
--- widget/js/puzzle.worker.js
+++ widget/js/puzzle.worker.js
@@ -31,7 +31,7 @@ function readUInt32LE(buffer, offset) {
 }
 
 function thresholdFromDifficulty(d) {
-    return (Math.pow(2, Math.floor((255.999 - d) / 8.0))) >>> 0;
+    return (Math.pow(2, (255.999 - d) / 8.0)) >>> 0;
 }
 
 function findSolution(threshold, puzzleIndex, debug) {
@@ -50,7 +50,7 @@ function findSolution(threshold, puzzleIndex, debug) {
             puzzleBuffer[length - 1 - 2] = j;
 
             for (let k = 0; k < 256; k++) {
-                puzzleBuffer[length - 1 - 1] = j;
+                puzzleBuffer[length - 1 - 1] = k;
 
                 for (let l = 0; l < 256; l++) {
                     puzzleBuffer[length - 1 - 0] = l;
