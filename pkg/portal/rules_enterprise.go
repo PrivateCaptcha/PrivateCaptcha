12593ae0e2e3758c39e4e099e259f3b9303700d3
Taras Kushnir <tk.dev@mailbox.org>
2026-03-06 18:11:50 +0100

    Cosmetic improvement

--- pkg/portal/rules_enterprise.go
+++ pkg/portal/rules_enterprise.go
@@ -339,7 +339,7 @@ func (s *Server) initRuleParsers() {
 		string(dbgen.RuleActionPropertyDifficultyGrowth):       difficultyGrowthActionParser,
 		string(dbgen.RuleActionPropertyBreak):                  breakActionParser,
 	}
-	s.ConditionPropertyDisplayNames = map[dbgen.RuleConditionProperty]string{
+	s.ConditionPropertyNames = map[dbgen.RuleConditionProperty]string{
 		dbgen.RuleConditionPropertyIPAddress:      "IP address",
 		dbgen.RuleConditionPropertyHTTPHeaderName: "HTTP Header Name",
 	}
