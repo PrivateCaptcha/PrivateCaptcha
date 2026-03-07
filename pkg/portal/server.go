a1876485e9f4e4ecb38f4b60893e89a3c1a57938
Taras Kushnir <tk.dev@mailbox.org>
2026-03-07 11:03:34 +0100

    Refactor rule registry

--- pkg/portal/server.go
+++ pkg/portal/server.go
@@ -137,39 +137,37 @@ func (ac *AlertRenderContext) ClearAlerts() {
 }
 
 type Server struct {
-	Store                  db.Implementor
-	TimeSeries             common.TimeSeriesStore
-	APIURL                 string
-	CDNURL                 string
-	Prefix                 string
-	IDHasher               common.IdentifierHasher
-	template               *Templates
-	XSRF                   *common.XSRFMiddleware
-	Sessions               *session.Manager
-	Mailer                 common.Mailer
-	Stage                  string
-	PlanService            billing.PlanService
-	PuzzleEngine           puzzle.Engine
-	Metrics                common.PortalMetrics
-	maintenanceMode        atomic.Bool
-	canRegister            atomic.Bool
-	SettingsTabs           []*SettingsTab
-	RateLimiter            ratelimit.HTTPRateLimiter
-	RenderConstants        interface{}
-	Jobs                   Jobs
-	PlatformCtx            interface{}
-	DataCtx                interface{}
-	CountryCodeHeader      common.ConfigItem
-	UserLimiter            api.UserLimiter
-	AuditLogsFunc          AuditLogsConstructor
-	AuditLogParser         AuditLogParser
-	SubscriptionLimits     db.SubscriptionLimits
-	EmailVerifier          common.EmailVerifier
-	TwoFactorDuration      time.Duration
-	LicenseService         common.LicenseService
-	ConditionParsers       map[string]ConditionFormParser
-	ActionParsers          map[string]ActionFormParser
-	ConditionPropertyNames map[string]string
+	Store              db.Implementor
+	TimeSeries         common.TimeSeriesStore
+	APIURL             string
+	CDNURL             string
+	Prefix             string
+	IDHasher           common.IdentifierHasher
+	template           *Templates
+	XSRF               *common.XSRFMiddleware
+	Sessions           *session.Manager
+	Mailer             common.Mailer
+	Stage              string
+	PlanService        billing.PlanService
+	PuzzleEngine       puzzle.Engine
+	Metrics            common.PortalMetrics
+	maintenanceMode    atomic.Bool
+	canRegister        atomic.Bool
+	SettingsTabs       []*SettingsTab
+	RateLimiter        ratelimit.HTTPRateLimiter
+	RenderConstants    interface{}
+	Jobs               Jobs
+	PlatformCtx        interface{}
+	DataCtx            interface{}
+	CountryCodeHeader  common.ConfigItem
+	UserLimiter        api.UserLimiter
+	AuditLogsFunc      AuditLogsConstructor
+	AuditLogParser     AuditLogParser
+	SubscriptionLimits db.SubscriptionLimits
+	EmailVerifier      common.EmailVerifier
+	TwoFactorDuration  time.Duration
+	LicenseService     common.LicenseService
+	Rules              *RuleRegistry
 }
 
 func (s *Server) createSettingsTabs() []*SettingsTab {
@@ -212,7 +210,7 @@ func (s *Server) Init(ctx context.Context, templateBuilder *TemplatesBuilder, gi
 	s.SettingsTabs = s.createSettingsTabs()
 	s.RenderConstants = NewRenderConstants()
 	s.AuditLogsFunc = s.CreateAuditLogsContext
-	s.initRuleParsers()
+	s.Rules = NewRuleRegistry()
 
 	platformCtx := &PlatformRenderContext{
 		GitCommit:      gitCommit,
