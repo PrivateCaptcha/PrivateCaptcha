e539d491fa5ea7bc362e5cf1de658751fe2e6920
Taras Kushnir <tk.dev@mailbox.org>
2026-05-13 10:08:49 +0200

    Fix Blake2b fallback when WebAssembly is not available

--- widget/js/blake2-wrapper.js
+++ widget/js/blake2-wrapper.js
@@ -2,22 +2,19 @@ import * as b2wasm from 'blake2b-wasm';
 import { blake2bInit, blake2bUpdate, blake2bFinal } from 'blakejs';
 
 function Blake2b(outlen, key, salt, personal) {
-    return blake2bInit(outlen, key);
+    this._ctx = blake2bInit(outlen, key);
 }
 
 Blake2b.prototype.update = function(input) {
-    // assert(input instanceof Uint8Array, 'input must be Uint8Array or Buffer')
-    blake2bUpdate(this, input);
+    blake2bUpdate(this._ctx, input);
     return this;
-}
+};
 
 Blake2b.prototype.digest = function(out) {
-    var buf = (!out) ? new Uint8Array(this.outlen) : out
-    // assert(buf instanceof Uint8Array, 'out must be "binary", "hex", Uint8Array, or Buffer')
-    // assert(buf.length >= this.outlen, 'out must have at least outlen bytes of space')
-    blake2bFinal(this, buf);
+    var buf = (!out) ? new Uint8Array(this._ctx.outlen) : out;
+    blake2bFinal(this._ctx, buf);
     return buf;
-}
+};
 
 Blake2b.prototype.final = Blake2b.prototype.digest;
 
