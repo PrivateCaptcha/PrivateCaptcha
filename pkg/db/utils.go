233762eda20a752ee2ccf70e6105a3f33aef1eee
Taras Kushnir <tk.dev@mailbox.org>
2026-06-26 12:08:03 +0300

    Fix typo

--- pkg/db/utils.go
+++ pkg/db/utils.go
@@ -487,7 +487,7 @@ func (sf *StoreArrayReader[TKey, T]) Read(ctx context.Context) ([]*T, error) {
 	if t, ok := data.([]*T); ok {
 		slog.Log(ctx, common.LevelTrace, "Read array through cache", "cacheKey", sf.CacheKey, "count", len(t))
 
-		if atomic.LoadInt32(&sf.readFlag) == 1 {
+		if wasRead {
 			if sf.TTL > 0 {
 				_ = sf.Cache.SetTTL(ctx, sf.CacheKey, sf.TTL)
 			}
