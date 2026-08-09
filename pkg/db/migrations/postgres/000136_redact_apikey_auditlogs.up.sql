8b21be6b940032a16826ec0d75274fd8e19307fe
Taras Kushnir <tk.dev@mailbox.org>
2026-08-09 11:35:37 +0300

    Fix audit logs migration

--- pkg/db/migrations/postgres/000136_redact_apikey_auditlogs.up.sql
+++ pkg/db/migrations/postgres/000136_redact_apikey_auditlogs.up.sql
@@ -1,6 +1,15 @@
 UPDATE backend.audit_logs
 SET
-    old_value = old_value - 'external_id',
-    new_value = new_value - 'external_id'
+    old_value = CASE
+        WHEN jsonb_typeof(old_value) = 'object' THEN old_value - 'external_id'
+        ELSE old_value
+    END,
+    new_value = CASE
+        WHEN jsonb_typeof(new_value) = 'object' THEN new_value - 'external_id'
+        ELSE new_value
+    END
 WHERE entity_table = 'apikeys'
-  AND (old_value ? 'external_id' OR new_value ? 'external_id');
+  AND (
+      (jsonb_typeof(old_value) = 'object' AND old_value ? 'external_id')
+      OR (jsonb_typeof(new_value) = 'object' AND new_value ? 'external_id')
+  );
