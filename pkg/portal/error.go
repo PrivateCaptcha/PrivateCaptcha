d5b6beea902d6952e55ca4cd2d81619986c769e4
Taras Kushnir <tk.dev@mailbox.org>
2026-08-09 10:23:54 +0300

    Bound portal error metric route labels

--- pkg/portal/error.go
+++ pkg/portal/error.go
@@ -109,7 +109,12 @@ func (s *Server) error(w http.ResponseWriter, r *http.Request) {
 func (s *Server) RedirectError(code int, w http.ResponseWriter, r *http.Request) {
 	url := s.RelURL(common.ErrorEndpoint + "/" + strconv.Itoa(code))
 	common.Redirect(url, code, w, r)
-	s.Metrics.ObserveHttpError(r.URL.Path, r.Method, code)
+
+	handlerID, _ := r.Context().Value(common.PathPatternContextKey).(string)
+	if handlerID == "" {
+		handlerID = "_unknown"
+	}
+	s.Metrics.ObserveHttpError(handlerID, r.Method, code)
 }
 
 func (s *Server) notFound(w http.ResponseWriter, r *http.Request) {
