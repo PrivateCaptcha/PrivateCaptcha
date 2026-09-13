0839306e9b2fe9d0e99c8ee50549577b6d0d5b67
Taras Kushnir <tk.dev@mailbox.org>
2026-09-13 21:56:56 +0300

    Use HTTP 4xx response for negative cache for properties

--- pkg/api/server_enterprise.go
+++ pkg/api/server_enterprise.go
@@ -158,7 +158,7 @@ func (s *Server) sendHTTPErrorResponse(err error, w http.ResponseWriter) {
 		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
 	case errAPIKeyScope, errInvalidAPIKey, errAPIKeyNotSet, errAPIKeyReadOnly, db.ErrPermissions:
 		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-	case db.ErrSoftDeleted:
+	case db.ErrSoftDeleted, db.ErrNegativeCacheHit:
 		http.Error(w, http.StatusText(http.StatusConflict), http.StatusConflict)
 	case context.DeadlineExceeded:
 		http.Error(w, http.StatusText(http.StatusGatewayTimeout), http.StatusGatewayTimeout)
