08f6206fe760e9deb684e93039d6a3d7dcc788ed
Taras Kushnir <tk.dev@mailbox.org>
2026-09-16 10:51:17 +0200

    Specify the extension schema explicitly

--- pkg/db/migrations/postgres/000139_cap_property_validity.up.sql
+++ pkg/db/migrations/postgres/000139_cap_property_validity.up.sql
@@ -1,5 +1,5 @@
 -- The salt rotation invalidates puzzles whose original validity is no longer retained.
 UPDATE backend.properties
 SET validity_interval = INTERVAL '24 hours',
-    salt = gen_random_bytes(8)
+    salt = backend.gen_random_bytes(8)
 WHERE validity_interval > INTERVAL '24 hours';
